b736f01e1919c97fbd6810d7ddcb7a9411849190
DANILO DA CRUZ MUNHE <21276321@CFP127ALUNO.LOCAL>
2022-03-04 16:33:59 -0300

    criando o arquivo route para fazer a comunicação entre view, controller e model

--- router.php
+++ router.php
@@ -1,8 +1,32 @@
 <?php
 
+    /*************************************************
+     * Objetivo: arquivo de rota, para segmentar as ações encaminhadas pela View
+     *  (dados de um form, listagem de dados, ação de excluir ou atualizar)
+     *  Esse arquivo será responsável por encaminhar a solicitações para a Controller
+     * 
+     * Autor: Danilo
+     * Data: 04/03/2022
+     * versão: 1.0
+     */
+
+    $action = (string) null;
+    $component = (string) null;
+
     //Validação para verificar se a requisição é um post de um formulário
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        echo('requisição do form');
+        
+        //recebendo dados via URl para saber quem está solicitando e qual ação será realizada
+        $component = strtoupper($_GET['component']);
+        $action = $_GET['action']; 
+
+        //Estrutura condicional para validar quem está solicitando algo para o router
+        switch($component){
+
+            case 'CONTATOS': 
+                echo('chamando a controller de contatos');
+            break;
+        }
     }
 
 
