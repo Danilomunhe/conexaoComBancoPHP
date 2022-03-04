1d23f2f225fa8887f49c4e84d22f8d2d1d696b2d
DANILO DA CRUZ MUNHE <21276321@CFP127ALUNO.LOCAL>
2022-03-04 17:00:13 -0300

    criando o arquivo route para fazer a comunicação entre view, controller e model

---
+++ controller/controllerContatos.php
@@ -0,0 +1,27 @@
+<?php
+    /****************************************************
+     * Objetivo: Arquivo responsável pela manipulação de dados de contatos
+     * Obs: Esse arquivo fará a ponte entre a View e a model
+     * Autor:Danilo
+     * Data:04/03
+     * Versão:1.0
+     ****************************************************/
+
+    //Função para recber dados da View e encaminhar para o Model(inserir)
+     function inserirContato(){
+
+     }
+
+      //Função para recber dados da View e encaminhar para o Model(atualizar)
+     function atualizarContato(){
+
+     }
+      //Função para realizar a exclusão de um dado
+     function excluirContato(){
+
+     }
+    //Função para solicitar os dados da model e encaminhar a lista de contatos para a View
+     function listarContato(){
+
+     }
+?>
